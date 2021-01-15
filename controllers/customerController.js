182a4f1aeec8260b62da65727ae80b81d9188e07
unknown <h.alhofufy@gmail.com>
2021-01-15 16:03:08 +0300

    make it better

---
+++ controllers/customerController.js
@@ -0,0 +1,32 @@
+const Customer = require('./../models/Customer')
+
+const customerController = {
+    test: async (req,res) => {
+        try {
+            res.json(req.body)
+            console.log(req.body)
+        } catch(e) {
+            res.json(e)
+        }
+    },
+    createCustomer: async (req,res) => {
+        try {
+            const customer = await Customer.create(req.body)
+            res.json(customer)
+            console.log(customer)
+        } catch(e) {
+            res.json(e)
+        }
+    },
+    getCustomer: async (req,res) => {
+        try {
+            const customers = await Customer.find()
+            res.json(customers)
+            console.log(customers)
+        } catch(e) {
+            res.json(e)
+        }
+    }
+}
+
+module.exports = customerController
---
+++ index.js
@@ -0,0 +1,40 @@
+//-----------------------------------------------------------------------
+// Setup
+//-----------------------------------------------------------------------
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const dotenv = require('dotenv') 
+dotenv.config()
+const customerController = require('./controllers/customerController')
+
+//-----------------------------------------------------------------------
+// Routes
+//-----------------------------------------------------------------------
+app.post('/customer', customerController.test)
+app.get('/customer', customerController.getCustomer)
+
+
+//-----------------------------------------------------------------------
+// DB connection
+//-----------------------------------------------------------------------
+const mongoURL = process.env.DB_HOST;
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('DB connected success')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('error', err)
+})
+//-----------------------------------------------------------------------
+// Port Connection
+//-----------------------------------------------------------------------
+app.listen(process.env.DB_PORT , ()=>{
+    console.log('Server listening to port ' + process.env.DB_PORT)
+})
